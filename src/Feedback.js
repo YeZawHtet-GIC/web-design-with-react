cf74da7768cd47882d7868630d16daa9e92b8bc6
YeZawHtet <danny@DESKTOP-G2JQFKU>
2024-01-23 22:49:35 +0630

    .

--- src/Feedback.js
+++ src/Feedback.js
@@ -3,63 +3,79 @@ import React from "react";
 export default function Feedback() {
   return (
     <div className="container-fluid py-5">
-      <div className="row">
-        <div className="col-lg-4">
-          <span className="caption">TESTIMONIALS</span>
-          <h3 className="heading">Our Customers Feedbck</h3>
-          <a
-            className="d-block text-center text-light"
-            style={{
-              width: "50px",
-              height: "40px",
-              lineHeight: "35px",
-              fontSize: "24px",
-              backgroundColor: "#cccccc",
-            }}
-          >
-            <svg
-              className="bi bi-arrow-left"
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4">
+            <span className="caption">TESTIMONIALS</span>
+            <h3 className="heading">Our Customers Feedbck</h3>
+            <a
+              className="d-inline-block me-3 text-center text-light"
+              style={{
+                width: "50px",
+                height: "40px",
+                lineHeight: "35px",
+                fontSize: "24px",
+                backgroundColor: "#cccccc",
+              }}
             >
-              <path
-                fill-rule="evenodd"
-                d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"
-              ></path>
-              <path
-                fill-rule="evenodd"
-                d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-              ></path>
-            </svg>
-          </a>
-        </div>
-        <div className="col-lg-6">
-          <div className="row row-cols-1 row-cols-lg-2">
-            <div className="col">
-              <blockquote>
-                “Separated they{" "}
-                <span class="highlight">
-                  live in Bookmarksgrove right at the coast of the Semantics
-                </span>
-                , a large language ocean. A small river named Duden flows by
-                their place and supplies it with the necessary regelialia. ”
-              </blockquote>
-            </div>
-            <div className="col">
-              <blockquote>
-                “Separated they{" "}
-                <span class="highlight">
-                  live in Bookmarksgrove right at the coast of the Semantics
-                </span>
-                , a large language ocean. A small river named Duden flows by
-                their place and supplies it with the necessary regelialia. ”
-              </blockquote>
-            </div>
+              <svg
+                className="bi bi-arrow-left"
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"
+                ></path>
+                <path
+                  fill-rule="evenodd"
+                  d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                ></path>
+              </svg>
+            </a>
+            <a
+              className="d-inline-block text-center text-light"
+              style={{
+                width: "50px",
+                height: "40px",
+                lineHeight: "35px",
+                fontSize: "24px",
+                backgroundColor: "#cccccc",
+              }}
+            >
+              <svg class="bi bi-arrow-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M10.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L12.793 8l-2.647-2.646a.5.5 0 0 1 0-.708z"></path>
+                <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5H13a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8z"></path>
+              </svg>
+            </a>
           </div>
-          {/* <div
+          <div className="col-lg-6">
+            <div className="row row-cols-1 row-cols-lg-2">
+              <div className="col">
+                <blockquote>
+                  “Separated they{" "}
+                  <span class="highlight">
+                    live in Bookmarksgrove right at the coast of the Semantics
+                  </span>
+                  , a large language ocean. A small river named Duden flows by
+                  their place and supplies it with the necessary regelialia. ”
+                </blockquote>
+              </div>
+              <div className="col">
+                <blockquote>
+                  “Separated they{" "}
+                  <span class="highlight">
+                    live in Bookmarksgrove right at the coast of the Semantics
+                  </span>
+                  , a large language ocean. A small river named Duden flows by
+                  their place and supplies it with the necessary regelialia. ”
+                </blockquote>
+              </div>
+            </div>
+            {/* <div
             id="carouselExampleIndicators"
             className="carousel slide"
             data-bs-ride="carousel"
@@ -115,6 +131,7 @@ export default function Feedback() {
               <span className="visually-hidden">Next</span>
             </button>
           </div> */}
+          </div>
         </div>
       </div>
     </div>
